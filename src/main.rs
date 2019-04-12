dc664026cdb892f28174e8afb0553268162dc417
Ihsahn <ihsahn@users.noreply.github.com>
2019-04-12 21:29:56 +0200

    Retrieve and display single http page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate reqwest;
+
 fn main() {
-    println!("Hello, world!");
+    let user = "username";
+    let passwd = "password";
+
+    let parent_path = "http://localhost:8000/builds/develop/";
+
+    let page = get_page(parent_path, user, passwd);
+    println!("Page: {:?}", page);
+}
+
+fn get_page(url: &str, user: &str, passwd: &str) -> String {
+    let client = reqwest::Client::new();
+    let mut rsp = client
+        .get(url)
+        .basic_auth(user, Some(passwd))
+        .send()
+        .unwrap();
+    let body = rsp.text().unwrap();
+    body
 }
