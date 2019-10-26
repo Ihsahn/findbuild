ee79d37463c98403ec383d3c0476455964ef1730
Ihsahn <ihsahn@users.noreply.github.com>
2019-10-26 09:27:32 +0200

    Specific build download

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate reqwest;
 extern crate regex;
 
-use reqwest::Client;
+use reqwest::{Client, Response};
 use regex::Regex;
+use std::fs::File;
+use std::io;
 
 fn main() {
     let user = "username";
@@ -10,21 +12,41 @@ fn main() {
 
     let parent_path = "http://localhost:8000/builds/develop/";
     let version_pattern = "(b\\d+)";
+    let platform = "linux64";
+    let package_pattern = "(findbuild_.*?snap)";
 
-    let page = get_page(parent_path, user, passwd);
-    println!("Page: {:?}", page);
+    let build_version_name = get_page_and_find_matching(user, passwd, version_pattern, parent_path);
 
-    let result = find_last_matching_url(&page, version_pattern);
-    println!("Result: {:?}", result);
+    let platform_path = parent_path.to_owned() + build_version_name.as_str() + "/" + platform;
+
+    let package_name = get_page_and_find_matching(user, passwd, package_pattern, &platform_path);
+
+    let specific_build_url = platform_path + "/" + &package_name;
+
+    println!("Full url: {:?}", specific_build_url);
+    let mut package_response = get(specific_build_url.as_str(), user, passwd);
+    let mut out = File::create(package_name).unwrap();
+    io::copy(&mut package_response, &mut out).unwrap();
 }
 
-fn get_page(url: &str, user: &str, passwd: &str) -> String {
+fn get_page_and_find_matching(user: &str, passwd: &str, pattern: &str, path: &str) -> String {
+    let platform_page = get_page(path, user, passwd);
+    let build_package_name = find_last_matching_url(&platform_page, pattern);
+    build_package_name.to_string()
+}
+
+fn get(url: &str, user: &str, passwd: &str) -> Response {
     let client = Client::new();
-    let mut rsp = client
+    let rsp = client
         .get(url)
         .basic_auth(user, Some(passwd))
         .send()
         .unwrap();
+    rsp
+}
+
+fn get_page(url: &str, user: &str, passwd: &str) -> String {
+    let mut rsp = get(url, user, passwd);
     let body = rsp.text().unwrap();
     body
 }
