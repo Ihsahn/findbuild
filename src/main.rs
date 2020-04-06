51824e6bbc7c2f8cf7dad33ba41b2ea172169544
Ihsahn <ihsahn@users.noreply.github.com>
2020-04-06 18:54:52 +0200

    Add ability to get parameters from command line or env. Process arbitrary long package tree paths

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,77 @@
 extern crate reqwest;
 extern crate regex;
+extern crate clap;
 
 use reqwest::{Client, Response};
 use regex::Regex;
 use std::fs::File;
 use std::io;
+use std::env;
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, App};
+use std::process::exit;
 
 fn main() {
-    let user = "username";
-    let passwd = "password";
+    let arguments = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("user")
+            .short("u")
+            .long("user")
+            .value_name("USER")
+            .takes_value(true))
+        .arg(Arg::with_name("password")
+            .short("p")
+            .long("password")
+            .value_name("PASSWORD")
+            .takes_value(true))
+        .arg(Arg::with_name("path")
+            .long("path")
+            .value_name("PATH")
+            .takes_value(true))
+        .get_matches();
 
-    let parent_path = "http://localhost:8000/builds/develop/";
-    let version_pattern = "(b\\d+)";
-    let platform = "linux64";
-    let package_pattern = "(findbuild_.*?snap)";
+    let user = arguments.value_of("user")
+        .map(|s| s.to_owned())
+        .or(env::var("FINDBUILD_USERNAME").ok())
+        .expect("No username specified");
+    let passwd = arguments.value_of("password")
+        .map(|s| s.to_owned())
+        .or(env::var("FINDBUILD_PASSWORD").ok())
+        .expect("No password specified");
+    let path = arguments.value_of("path")
+        .map(|s| s.to_owned())
+        .or(env::var("FINDBUILD_PATH").ok())
+        .expect("No path specified");
 
-    let build_version_name = get_page_and_find_matching(user, passwd, version_pattern, parent_path);
+    let mut search_paths = path.split("|");
 
-    let platform_path = parent_path.to_owned() + build_version_name.as_str() + "/" + platform;
+    println!("Username used: {:?}", user);
+    println!("Path(s) used: ");
+    search_paths.clone().for_each(|p| println!("  {:?}", p));
 
-    let package_name = get_page_and_find_matching(user, passwd, package_pattern, &platform_path);
+    if search_paths.clone().count() < 2 {
+        println!("Path should contain at least two segments");
+        exit(-1);
+    }
 
-    let specific_build_url = platform_path + "/" + &package_name;
+    let mut path = search_paths.next().unwrap().to_string();
+    let mut last_part = String::from("");
+    loop {
+        match search_paths.next() {
+            None => { break; }
+            Some(pattern) => {
+                println!("looking for {} at {}", pattern, path);
+                last_part = get_page_and_find_matching(&user, &passwd, pattern, &path);
+                path = format!("{}/{}", path, last_part.as_str());
+            }
+        }
+    }
+    let specific_build_url = path;
+    let package_name = last_part;
 
     println!("Full url: {:?}", specific_build_url);
-    let mut package_response = get(specific_build_url.as_str(), user, passwd);
+    let mut package_response = get(specific_build_url.as_str(), &user, &passwd);
     let mut out = File::create(package_name).unwrap();
     io::copy(&mut package_response, &mut out).unwrap();
 }
